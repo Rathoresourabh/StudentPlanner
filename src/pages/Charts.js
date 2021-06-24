282993d5247f128312722a240cc8e0b3eba3b16c
Rathoresourabh <sourabhrathour1999@gmail.com>
2021-06-24 14:13:55 +0530

    changes

--- src/pages/Charts.js
+++ src/pages/Charts.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React ,{useContext} from 'react'
+import {UserContext} from '../App'
+import {useHistory} from 'react-router-dom'
+import {Typography ,Button} from "@material-ui/core"
 
 function Charts() {
+    let {user} = useContext(UserContext);
+    let history = useHistory();
     return (
+        
+        <Typography variant="h4" color="primary" align="center">
+        Please go back to Home Page and fill in your details {user.displayName} To show your Chart
+
         <div>
-            heybboii
+        <Button variant="outlined" color="primary" align="center" onClick = {function() {
+            history.push('/')
+        }}>
+            Go Back To Home Page
+            </Button>
         </div>
+        
+        </Typography>
+
+        
     )
 }
+
 
 export default Charts
